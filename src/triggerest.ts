02c6e1f185347c2c15e7cf9443f36e1ee65d43c4
Oktay Şenkan <oktay.senkan@gmail.com>
2020-10-04 04:09:13 +0300

    feat(src/triggerest.ts): identifier parameter is added

--- src/triggerest.ts
+++ src/triggerest.ts
@@ -2,21 +2,33 @@ import { EventEmitter2 } from 'eventemitter2';
 import * as _ from 'lodash';
 
 class Triggerrest extends EventEmitter2 {
+  readonly func: () => Promise<any[]>;
+
+  readonly identifier: string;
+
+  readonly timeout: number;
+
   lastValue: any[] = [];
 
   currentValue: any[] = [];
 
-  readonly func: () => Promise<any[]>;
+  processCount = 0;
 
   /**
    * Returns a new Triggerest instance
    * @param func Function who returns array
    * @param timeout Function call delay
    */
-  constructor(func: () => Promise<any[]>, timeout = 60 * 1000) {
+  constructor(
+    func: () => Promise<any[]>,
+    identifier: string,
+    timeout = 60 * 1000
+  ) {
     super();
 
     this.func = func;
+    this.identifier = identifier;
+    this.timeout = timeout;
 
     setInterval(() => {
       this.calculateDifference();
@@ -30,25 +42,37 @@ class Triggerrest extends EventEmitter2 {
 
     this.emit('result', result);
 
-    const addedItems = _.difference(this.currentValue, this.lastValue);
-    const removedItems = _.difference(this.lastValue, this.currentValue);
+    const addedItems = _.differenceBy(
+      this.currentValue,
+      this.lastValue,
+      this.identifier
+    );
 
+    const removedItems = _.differenceBy(
+      this.lastValue,
+      this.currentValue,
+      this.identifier
+    );
+
+    const isFirstTime = this.processCount === 0;
     const isAnyItemAdded = Boolean(addedItems.length);
     const isAnyItemRemoved = Boolean(removedItems.length);
 
-    if (isAnyItemAdded || isAnyItemRemoved) {
+    if ((isAnyItemAdded || isAnyItemRemoved) && !isFirstTime) {
       this.emit('changed', addedItems, removedItems, result);
     }
 
-    if (isAnyItemAdded) {
+    if (isAnyItemAdded && !isFirstTime) {
       this.emit('added', addedItems, result);
     }
 
-    if (isAnyItemRemoved) {
+    if (isAnyItemRemoved && !isFirstTime) {
       this.emit('removed', removedItems, result);
     }
 
     this.lastValue = this.currentValue;
+
+    this.processCount++;
   }
 }
 
